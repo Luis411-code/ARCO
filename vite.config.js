ed266d8fcec6ad72ecb5675247beee8a93e02392
Luis411-code <lg1855226@gmail.com>
2026-07-27 10:48:36 -0400

    cambioss

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Si usas GitHub Pages, esto sería '/ARCO/', pero para Vercel no hace falta
-  // base: '/',
-  server: {
-    hmr: {
-      overlay: false
-    }
+  define: {
+    'process.env': {}
+  },
+  optimizeDeps: {
+    include: ['mongodb']
   }
-})
+});
